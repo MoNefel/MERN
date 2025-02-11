2ecbd1d63982b2aa6d9f699bf19516675cbf3738
Mohamed Akez Nefel <mohamed.akez@nefel.education>
2025-02-11 09:01:15 +0100

    Fix an error

--- javaScript/npm/Core/Node_Dates/isodate-formatter/index.js
+++ javaScript/npm/Core/Node_Dates/isodate-formatter/index.js
@@ -1,5 +1,5 @@
 import { formatISODate, formatRelativeTime, formatFullDate, formatShortDate } from './utils/formatDate.js';
-import chalk from 'chalk';
+import chalk from 'chalk'; // chalk is a library that allows you to colorize your console output
 
 const sampleISODate = "2023-10-05T14:48:00.000Z";
 
--- javaScript/npm/Core/Node_Dates/isodate-formatter/utils/formatDate.js
+++ javaScript/npm/Core/Node_Dates/isodate-formatter/utils/formatDate.js
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
+import relativeTime from "dayjs/plugin/relativeTime.js";
 
 dayjs.extend(relativeTime);
 
@@ -7,17 +7,14 @@ export function formatISODate(isoDate) {
     return dayjs(isoDate).format("MMMM D, YYYY, h:mm:ss A [UTC]");
 }
 
-
 export function formatRelativeTime(isoDate) {
     return dayjs(isoDate).fromNow();
 }
 
-
 export function formatFullDate(isoDate) {
     return dayjs(isoDate).format("dddd, MMMM D, YYYY");
-  }
-
+}
 
 export function formatShortDate(isoDate) {
     return dayjs(isoDate).format("MM/DD/YYYY");
-  }
+}

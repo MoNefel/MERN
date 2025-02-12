be79e88e662e3625c11538bdeb380050d92ad0c2
Mohamed Akez Nefel <mohamed.akez@nefel.education>
2025-02-12 13:37:45 +0100

    Ternaries, .map & toggling

--- React/first_react_app/first-react/src/components/DisplayAlbums.jsx
+++ React/first_react_app/first-react/src/components/DisplayAlbums.jsx
@@ -1,20 +1,36 @@
 const Display = (props) => {
 
-    const { albums } = props
+    const { albums, setAlbums } = props
     // const { albumName, genre, releaseYear, isExplicit } = album
 
+    const toggleListened = (id) => {
+        const updatedList = albums.map((album)=>{
+            if(id === album.id){
+                album.listened = !album.listened
+            }
+            return album
+        })
+        console.log(updatedList)
+        setAlbums(updatedList)
+    }
+
     return (
-        <div>
+        <div >
             <h1>Albums</h1>
+            <div className="d-flex border p-3">
             {albums.map((album) => (
                 <div key={album.id}>
                     <h2>ID : {album.id}</h2>
                     <h3>NAME : {album.albumName}</h3>
                     <p>GENRE : {album.genre}</p>
                     <p>RELEASE YEAR : {album.releaseYear}</p>
-                    <p>{album.isExplicit ? "Explicit" : "Not Explicit"}</p>
+                    <p>Explicit : {album.isExplicit ? "Parental Advisory" : "Not Explicit"}</p>
+                    <label >Listened ?</label>
+                    <input type="checkbox" name="isExplicit" defaultChecked={album.listened} onClick={() => toggleListened(album.id)} />
                 </div>
+                
             ))}
+            </div>
         </div>
     )
 }
--- React/first_react_app/first-react/src/components/NewForm.jsx
+++ React/first_react_app/first-react/src/components/NewForm.jsx
@@ -1,39 +1,65 @@
 import { useState } from "react"
 import {v4 as uuidv4 } from 'uuid'
 
+const date = new Date()
+const formattedDate = date.toISOString().split('T')[0]
+
 const NewForm = (props) => {
 
         const { albums, setAlbums } = props
         const [albumName, setAlbumName] = useState('')
         const [genre, setGenre] = useState('')
-        const [releaseYear, setReleaseYear] = useState('')
+        const [releaseYear, setReleaseYear] = useState(formattedDate)
         const [isExplicit, setIsExplicit] = useState(false)
+        const [isSubmitted, setIsSubmitted] = useState(false)
+        const [listened, setListened] = useState(false)
 
         
     
         const handleSubmit = (e) => {
             e.preventDefault()
-            const newAlbum = { albumName, genre, releaseYear, isExplicit, id: uuidv4() }
+            const newAlbum = { albumName, genre, releaseYear, isExplicit, id: uuidv4(), listened }
             setAlbums([...albums, newAlbum])
     
             console.log(newAlbum)
     
             setAlbumName('')
             setGenre('')
-            setReleaseYear('')
+            setReleaseYear(formattedDate)
+            setIsExplicit(false)
+            setIsSubmitted(true)
+            //alert('Thanks for posting your album!')
+            setTimeout(() => {
+                setIsSubmitted(false)
+            }, 3000)
         }
 
     return(
         <div>
             <h2>Album Form</h2>
+            {
+                isSubmitted 
+                ? <h3>Thanks for submitted the form</h3> 
+                : <h3>Please fill out the form</h3>
+            }
             <form onSubmit={handleSubmit}>
                 <div>
                     <label>Album Name:</label>
                     <input type="text" name="albumName" value={albumName} onChange={(e) => setAlbumName(e.target.value)} />
+                    {
+                        albumName.length < 3 
+                        ? <p>Album name must be at least 3 characters</p> 
+                        : ''
+                    }
                 </div>
                 <div>
                     <label>Genre:</label>
                     <input type="text" name="genre" value={genre} onChange={(e) => setGenre(e.target.value)} />
+                    {
+                        genre.length < 3 
+                        ? <p>Genre must be at least 3 characters</p> 
+                        : ''
+                    }
                 </div>
                 <div>
                     <label>Release Year:</label>

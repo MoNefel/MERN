55524ace86e059f215ff2742e25cb0c463d97456
Mohamed Akez Nefel <mohamed.akez@nefel.education>
2025-02-13 11:01:19 +0100

    Code Review Starter Folder

---
+++ React/functional_components/Code-Review/starter/src/App.jsx
@@ -0,0 +1,59 @@
+import './App.css'
+
+
+function App() {
+
+  const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "", 
+      soldOut: true,
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName:"",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "",
+      soldOut: true,
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "",
+      soldOut: false,
+    },
+  ];
+
+
+  return (
+    < >
+   
+    </>
+  )
+}
+
+export default App

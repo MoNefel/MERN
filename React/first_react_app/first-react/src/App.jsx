daacc66853a6ad1bbe9e02db688575bbc58936ea
Mohamed Akez Nefel <mohamed.akez@nefel.education>
2025-02-17 09:03:47 +0100

    Form

--- React/first_react_app/first-react/src/App.jsx
+++ React/first_react_app/first-react/src/App.jsx
@@ -1,30 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import Bench from './components/Bench'
-import Display from './components/DisplayAlbums'
-import Form from './components/Form'
-import Header from './components/Header'
-import NewForm from './components/NewForm'
-import Player from './components/Player'
-import Team from './components/Team'
-
+import { useState } from "react";
+import "./App.css";
+import Bench from "./components/Bench";
+import DisplayAlbums from "./components/DisplayAlbums";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import NewForm from "./components/NewForm";
+import Player from "./components/Player";
+import Team from "./components/Team";
 
 const App = () => {
-
-const[albums, setAlbums] = useState([])
+  const [albums, setAlbums] = useState([]);
 
   return (
-<div className='w-50 mx-auto'>
-<NewForm albums={albums} setAlbums={setAlbums} />
-<Display albums={albums} setAlbums={setAlbums}/>
-{/* <Form/>
+    <div className="w-50 mx-auto">
+      <NewForm albums={albums} setAlbums={setAlbums} />
+      <DisplayAlbums albums={albums} setAlbums={setAlbums} />
+      {/* <Form/>
 <Header/>
 <Team/>
 <Player name={"Med Akez"} position={"Forward"} number={55} points={32} />
 <Player name={"Akamensi"} position={"backward"} number={22} points={33} />
 <Bench/> */}
-</div>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
--- React/first_react_app/first-react/src/components/DisplayAlbums.jsx
+++ React/first_react_app/first-react/src/components/DisplayAlbums.jsx
@@ -1,38 +1,44 @@
-const Display = (props) => {
+const DisplayAlbums = (props) => {
+  const { albums, setAlbums } = props;
+  // const { albumName, genre, releaseYear, isExplicit } = album
 
-    const { albums, setAlbums } = props
-    // const { albumName, genre, releaseYear, isExplicit } = album
+  const toggleListened = (id) => {
+    const updatedList = albums.map((album) => {
+      if (id === album.id) {
+        album.listened = !album.listened;
+      }
+      return album;
+    });
+    console.log(updatedList);
+    setAlbums(updatedList);
+  };
 
-    const toggleListened = (id) => {
-        const updatedList = albums.map((album)=>{
-            if(id === album.id){
-                album.listened = !album.listened
-            }
-            return album
-        })
-        console.log(updatedList)
-        setAlbums(updatedList)
-    }
+  return (
+    <div>
+      <h1>Albums</h1>
+      <div className="d-flex border p-3">
+        {albums.map((album) => (
+          <div key={album.id}>
+            <h2>ID : {album.id}</h2>
+            <h3>NAME : {album.albumName}</h3>
+            <p>GENRE : {album.genre}</p>
+            <p>RELEASE YEAR : {album.releaseYear}</p>
+            <p>
+              Explicit :{" "}
+              {album.isExplicit ? "Parental Advisory" : "Not Explicit"}
+            </p>
+            <label>Listened ?</label>
+            <input
+              type="checkbox"
+              name="isExplicit"
+              defaultChecked={album.listened}
+              onClick={() => toggleListened(album.id)}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div >
-            <h1>Albums</h1>
-            <div className="d-flex border p-3">
-            {albums.map((album) => (
-                <div key={album.id}>
-                    <h2>ID : {album.id}</h2>
-                    <h3>NAME : {album.albumName}</h3>
-                    <p>GENRE : {album.genre}</p>
-                    <p>RELEASE YEAR : {album.releaseYear}</p>
-                    <p>Explicit : {album.isExplicit ? "Parental Advisory" : "Not Explicit"}</p>
-                    <label >Listened ?</label>
-                    <input type="checkbox" name="isExplicit" defaultChecked={album.listened} onClick={() => toggleListened(album.id)} />
-                </div>
-                
-            ))}
-            </div>
-        </div>
-    )
-}
-
-export default Display
+export default DisplayAlbums;
